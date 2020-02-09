cffa7f7ea1e53b2b1c3131822f4b1e8813af52b4
haxul <s.s.starodubov@yandex.ru>
2020-02-09 20:59:22 +0400

    init

---
+++ src/main/java/com/haxul/computing/ComputingApplication.java
@@ -0,0 +1,13 @@
+package com.haxul.computing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComputingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haxul/computing/ComputingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haxul.computing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
