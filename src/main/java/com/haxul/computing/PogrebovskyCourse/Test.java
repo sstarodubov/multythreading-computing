7b19002218e1a66354c497c72d0b18df729a242a
haxul <s.s.starodubov@yandex.ru>
2020-02-15 23:42:23 +0400

    init

--- src/main/java/com/haxul/computing/PogrebovskyCourse/Test.java
+++ src/main/java/com/haxul/computing/PogrebovskyCourse/Test.java
@@ -1,19 +1,11 @@
 package com.haxul.computing.PogrebovskyCourse;
 
-import com.sun.jmx.remote.internal.ArrayQueue;
-
-import java.util.Queue;
-import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Test {
     public static void main(String[] args) {
-        Queue<Integer> queue = (Queue<Integer>) new ArrayQueue<Integer>(2);
-        queue.add(10);
-        queue.add(23);
-        System.out.println(queue.remove());
-        for (Integer num: queue) {
-            System.out.println(num);
-        }
-
+        AtomicInteger atomicInteger = new AtomicInteger();
+        atomicInteger.addAndGet(5);
+        
     }
 }
